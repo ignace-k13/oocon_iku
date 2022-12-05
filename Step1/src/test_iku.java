faed9a340d13ad03ae70760110c09440fcd60f03
ignace k <ignace.k13@gmail.com>
2022-12-05 12:24:10 +0100

    test iku

--- Step1/src/test_iku.java
+++ Step1/src/test_iku.java
@@ -1,7 +1,7 @@
 public class test_iku {
 
     public static void main(String[] args) {
-        System.out.println("Hello from ignace");
+        System.out.println("Hello from Ignace");
 
     }
 }
